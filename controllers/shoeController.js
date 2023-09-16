8897cf6c43c5a5bd3f95244ffd17da3033e7a3e6
Stefan <stefanseifeddine@gmail.com>
2023-09-16 17:21:20 +0100

    Add shoe form methods

--- controllers/shoeController.js
+++ controllers/shoeController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require("express-validator");
 const Shoe = require("../models/shoe");
 const Brand = require("../models/brand");
 const Type = require("../models/type");
@@ -43,7 +44,7 @@ exports.shoe_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific shoe.
 exports.shoe_detail = asyncHandler(async (req, res, next) => {
-    // Get details of shoes, shoe instances for specific book
+    // Get details of shoes, shoe instances for specific shoe
     const [shoe, shoeInstances] = await Promise.all([
         Shoe.findById(req.params.id).populate("brand").populate("type").exec(),
         ShoeInstance.find({ shoe: req.params.id }).exec(),
@@ -65,13 +66,92 @@ exports.shoe_detail = asyncHandler(async (req, res, next) => {
 
 // Display shoe create form on GET.
 exports.shoe_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Shoe create GET");
+    // Get all brands and types, which we can use for adding to our shoe.
+    const [allBrands, allTypes] = await Promise.all([
+        Brand.find().exec(),
+        Type.find().exec(),
+    ]);
+
+    res.render("shoe_form", {
+        title: "Create Shoe",
+        brands: allBrands,
+        types: allTypes,
+    });
 });
 
 // Handle shoe create on POST.
-exports.shoe_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Shoe create POST");
-});
+exports.shoe_create_post = [
+    // Convert the type to an array.
+    (req, res, next) => {
+        if (!(req.body.type instanceof Array)) {
+            if (typeof req.body.type === "undefined") req.body.type = [];
+            else req.body.type = new Array(req.body.type);
+        }
+        next();
+    },
+
+    // Validate and sanitize fields.
+    body("name", "Name must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("brand", "Brand must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("description", "Description must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("price", "Price must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("type.*").escape(),
+    // Process request after validation and sanitization.
+
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a Shoe object with escaped and trimmed data.
+        const shoe = new Shoe({
+            name: req.body.name,
+            brand: req.body.brand,
+            description: req.body.description,
+            price: req.body.price,
+            type: req.body.type,
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages.
+
+            // Get all brands and types for form.
+            const [allBrands, allTypes] = await Promise.all([
+                Brand.find().exec(),
+                Type.find().exec(),
+            ]);
+
+            // Mark our selected types as checked.
+            for (const type of allTypes) {
+                if (shoe.type.includes(type._id)) {
+                    type.checked = "true";
+                }
+            }
+            res.render("shoe_form", {
+                title: "Create Shoe",
+                brands: allBrands,
+                types: allTypes,
+                shoe: shoe,
+                errors: errors.array(),
+            });
+        } else {
+            // Data from form is valid. Save shoe.
+            await shoe.save();
+            res.redirect(shoe.url);
+        }
+    }),
+];
 
 // Display shoe delete form on GET.
 exports.shoe_delete_get = asyncHandler(async (req, res, next) => {
